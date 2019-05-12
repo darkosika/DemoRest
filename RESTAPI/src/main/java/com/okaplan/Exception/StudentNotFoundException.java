592a7b1160bbcee18478dbaffe6584d737fb5159
OSMAN <osmnkpln03ozgurluk@gmail.com>
2019-05-12 13:18:51 +0300

    Exception Handle is added

---
+++ RESTAPI/src/main/java/com/okaplan/Exception/StudentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.okaplan.Exception;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
--- RESTAPI/src/main/java/com/okaplan/demo/rest/RestDemoController.java
+++ RESTAPI/src/main/java/com/okaplan/demo/rest/RestDemoController.java
@@ -3,24 +3,45 @@ package com.okaplan.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.okaplan.Exception.StudentNotFoundException;
 import com.okaplan.demo.Entity.Student;
+import com.okaplan.demo.Entity.StudentErrorResponse;
 
 @RestController
 @RequestMapping("/api")
 public class RestDemoController {
 	
+	private List<Student> _Students;
+	
 	@GetMapping("/students") 
 	public List<Student> getStudents() {
-		List<Student> _Students=new ArrayList<>();
+		return _Students;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		if( (studentId > _Students.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Such a student not exist");
+		}
+		return _Students.get(studentId);
+		
+	}
+	@PostConstruct
+	public void populateList(){
+		_Students=new ArrayList<>();	
 		_Students.add(new Student("Osman", "Kaplan"));
 		_Students.add(new Student("Oktay", "Kaplan"));
 		_Students.add(new Student("Gizem", "Kaplan"));
 		_Students.add(new Student("Esra", "Kaplan"));
-		
-		return _Students;
 	}
 }
